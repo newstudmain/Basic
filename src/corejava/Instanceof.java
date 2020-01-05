c067b8a15f438ddb71d429cf7005650f967cc09c
newstudmain <2192488518@qq.com>
2020-01-06 00:00:37 +0800

    .

--- src/corejava/Instanceof.java
+++ src/corejava/Instanceof.java
@@ -1,5 +1,21 @@
 package corejava;
 
+/*
+ * instanceof 运算符
+ * 		在运行时指出对象是否是特定类的一个实例,返回一个布尔值。
+ * 			result[bool] = object[obj] /instanceof/ class[cla]
+ * 		
+ * */
 public class Instanceof {
 
+	public static void main(String[] args) {
+
+
+	}
 }
+
+interface iA{}
+
+class iB implements iA{}
+
+class iC extends iB {}
